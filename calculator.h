71aa204f1f8d0789268a32639fb99f8603926d46
Lux-ex-tenebris <kostyanikovvladik@gmail.com>
2020-02-23 21:48:58 +0200

    add division function

--- calculator.h
+++ calculator.h
@@ -6,7 +6,9 @@ class Calculator
     public:
 
         int Add (double, double);
+	int Sub (double, double);
         int Mul (double, double);
+	int Div (double, double);
 };
 
 #endif//CALCULATOR_H
